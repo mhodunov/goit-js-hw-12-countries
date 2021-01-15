be167183519cf6cd55e8298d620ad6d19260e8af
Maksym Hodunov <m.godunov@yandex.ru>
2021-01-15 16:57:06 +0200

    Adds some fixes

--- src/js/pnotify-error.js
+++ src/js/pnotify-error.js
@@ -4,11 +4,15 @@ import '../../node_modules/@pnotify/core/dist/BrightTheme.css';
 
 export default function showError(errorMessage) {
   error({
+    type: 'error',
     text: errorMessage,
     delay: 3000,
     closer: false,
     closerHover: true,
     sticker: false,
+    addClass: 'notification',
+    icon: false,
+    width: '250px',
   });
 }
 
